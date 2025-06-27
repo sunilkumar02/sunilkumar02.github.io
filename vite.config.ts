7ca690ac698dab7f224d8a0de7d690a0db7b8761
sunilkumarAnnalect <sunil.kumar2+omcitcld@annalect.com>
2025-06-27 15:35:04 +0530

    update config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sunilkumar02.github.io/', 
+  build: {
+    outDir: 'dist', 
+  },
   server: {
     port: 3000,
     open: true
